3d2b67abe94e8f56ec68fa7d9fafc8b51f192bdb
Emiliano Kosh <mnnnkosh@gmail.com>
2022-02-18 17:22:28 -0300

    [Swagger] Implementation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
+    const swaggerDocument = require('./Configs/openapi.json');
+    SwaggerModule.setup('/', app, swaggerDocument);
     const port = configService.get('PORT');
     await app.listen(port);
 }
